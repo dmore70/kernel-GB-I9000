a562362589d885b76d33527e5e2ec07ed14635df
Davide Moressa <davidmore@inwind.it>
2013-08-13 13:57:28 +0200

    init: main.c - disable config kernel debug

--- init/main.c
+++ init/main.c
@@ -82,9 +82,7 @@
 #include <asm/smp.h>
 #endif
 
-#ifdef CONFIG_KERNEL_DEBUG_SEC
-#include <linux/kernel_sec_common.h>
-#endif
+
 
 static int kernel_init(void *);
 
@@ -710,9 +708,6 @@ asmlinkage void __init start_kernel(void)
 
 	ftrace_init();
 
-#ifdef CONFIG_KERNEL_DEBUG_SEC
-	kernel_sec_init();
-#endif
 
 	/* Do the rest non-__init'ed, we're now alive */
 	rest_init();
@@ -928,9 +923,6 @@ static int __init kernel_init(void * unused)
 		prepare_namespace();
 	}
 
-#ifdef CONFIG_KERNEL_DEBUG_SEC
-	kernel_sec_set_build_info();
-#endif
 
 	/*
 	 * Ok, we have completed the initial bootup, and

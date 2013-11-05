fab08b665b8fe4c3d440f5d4780aab8538090bbf
Davide Moressa <davidmore@inwind.it>
2013-11-05 15:26:09 +0100

    Initramfs: add command for delete a possible existing Semaphore.apk file

--- initramfs/sbin/boot/20_install_su.sh
+++ initramfs/sbin/boot/20_install_su.sh
@@ -1,20 +1,29 @@
 echo "Checking su and Superuser ....."
 
 install_su()
-{
+{          
+        echo "Install (su) and (superser)"  
 	/sbin/busybox mount -o remount,rw / /
 	/sbin/busybox mount -o remount,rw /system
+
 	/sbin/busybox rm /system/xbin/su
 	/sbin/busybox rm /system/app/Superuser*.apk
 	/sbin/busybox rm /data/app/Superuser*.apk
 	/sbin/busybox rm /system/app/com.noshufou.android.su*.apk
 	/sbin/busybox rm /data/app/com.noshufou.android.su*.apk
-	/sbin/busybox rm /data/data/com.noshufou.android.su*
+	/sbin/busybox rm -r /data/data/com.noshufou.android.su*
 	/sbin/busybox cp /res/misc/su /system/xbin/su
 	/sbin/busybox cp /res/misc/Superuser.apk /system/app/Superuser.apk
 	/sbin/busybox chown 0.0 /system/xbin/su
 	/sbin/busybox chmod 6755 /system/xbin/su
 	/sbin/busybox rm -r /system/fproot
+         /sbin/busybox rm -r /system/cfroot
+	/sbin/busybox rm -r /data/cfroot
+	/sbin/busybox rm -r /data/fproot
+	/sbin/busybox rm /system/app/Semaphore.apk
+	/sbin/busybox rm /data/dalvik-cache/*semaphore.apk*
+
+	
 	/sbin/busybox mount -o remount,ro / /
 	/sbin/busybox mount -o remount,ro /system
 }
